9b9e8f0f2063de5ad0960bb43c7a9cc484446413
SilasKumi <silaskumi4@gmail.com>
2026-02-16 09:07:18 +0000

    feat:provide retrieve data endpoints on note controller

--- src/main/java/com/amalitech/notesApi/controller/NoteController.java
+++ src/main/java/com/amalitech/notesApi/controller/NoteController.java
@@ -8,10 +8,9 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController("api/v1/notes")
@@ -33,5 +32,15 @@ public class NoteController {
 
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Note> getNotes() {
+        return noteService.getAllNotes();
+    }
+
+    @GetMapping("/{id}")
+    public Note getNote(@PathVariable Long id) {
+        return noteService.getNoteById(id);
     }
 }
