469e96ff6c6fdacb9d9a90c9997a5dc89665b906
SilasKumi <silaskumi4@gmail.com>
2026-02-16 10:06:21 +0000

    feat:add method to update note in service interface  and implement it

--- src/main/java/com/amalitech/notesApi/service/interfaces/NoteServiceInterface.java
+++ src/main/java/com/amalitech/notesApi/service/interfaces/NoteServiceInterface.java
@@ -9,4 +9,5 @@ public interface  NoteServiceInterface {
     Note createNote(NoteRequest note);
     List<Note> getAllNotes();
     Note getNoteById(Long id);
+    Note updateNote(Long id, NoteRequest note);
 }
